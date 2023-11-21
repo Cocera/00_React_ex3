9321e24885b213097ea3bff99c0ef372de786009
Cocera <alcocera@gmail.com>
2023-11-21 13:12:05 +0100

    counter structura and added to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from 'react'
 import './App.css'
+import Counter from './components/Counter/Counter'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Vite + React</h1>
+      < Counter />
     </>
   )
 }
---
+++ src/components/Counter/Counter.jsx
@@ -0,0 +1,7 @@
+const Counter = () => {
+    return <main>
+        <h1>The counter is here</h1>
+    </main>
+};
+
+export default Counter;
