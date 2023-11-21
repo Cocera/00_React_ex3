9259f86c6bb188c9fa0688771dcc8b2656c5382f
Cocera <alcocera@gmail.com>
2023-11-21 14:45:53 +0100

    counter restart & style

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,12 @@ import Counter from './components/Counter/Counter'
 function App() {
 
   return (
-    <>
-      < Counter counter={5}/>
-    </>
+    <section>
+      < Counter counter={0} countValue={1} />
+      < Counter counter={0} countValue={2} />
+      < Counter counter={0} countValue={5} />
+      < Counter counter={0} countValue={10} />
+    </section>
   )
 }
 
--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,22 +1,28 @@
 import React, { useState } from "react";
+import './Counter.css' 
 
 const Counter = (props) => {
     const [counter, setCounter] = useState(props.counter);
 
     const increase = () => {
-        setCounter(counter +1);
+        setCounter(counter + props.countValue);
     };
 
     const decrease = () => {
-        setCounter(counter -1);
+        setCounter(counter - props.countValue);
     };
 
-    return <main>
-        <h2>The counter is here</h2>
-        <h1>Counter: {counter}</h1>
-        <button onClick={() => increase()}>+</button>
+    const restart = () => {
+        setCounter(0);
+    };
+
+    return <article>
+        <h3>Counter <span>x{props.countValue}</span>: </h3>
+        <h1>{counter}</h1>
         <button onClick={() => decrease()}>-</button>
-    </main>
+        <button onClick={() => increase()}>+</button>
+        <button onClick={() => restart()}>Restart</button>
+    </article>
 };
 
 export default Counter;
